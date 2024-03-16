6f2af623d4fd65359c4f17bc5c184ec2e0258a43
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 19:41:06 -0300

    Neste commit estão a explicação do metodo unshift e seu primeiro exemplo

---
+++ unshift.js
@@ -0,0 +1,8 @@
+//Este é o metodo unshift
+//Ele permite adicionar novos elementos ao inicio de um array, fazendo que os outros elementos existentes avancem para o lado direito
+//exemplo 1:
+let numeros = ["1", "22", "333", "4444"];
+numeros.unshift(0);
+
+console.log(numeros);
+//output: [ 0, '1', '22', '333', '4444' ]
