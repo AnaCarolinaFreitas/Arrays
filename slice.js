6dc50f48088e4de12e8f6e19b6c9e9fbfff51cd0
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 20:14:51 -0300

    Este é o metodo slice, junto a ele está o primeiro exemplo de utilização

---
+++ slice.js
@@ -0,0 +1,8 @@
+//Este é o metodo slice
+//O slice seleciona valores determinados por um parâmetro em um novo array
+//Exemplo 1:
+let frutas = ["Banana", "Limão", "Abacaxi", "Morango", "Melancia"];
+let citricas = frutas.slice(1, 4);
+
+console.log(citricas);
+//output: [ 'Limão', 'Abacaxi', 'Morango' ]
