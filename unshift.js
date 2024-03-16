513ccec4db2bc890b6f282b85884aea403912f56
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 19:42:15 -0300

    Aqui temos o segundo exemplo

--- unshift.js
+++ unshift.js
@@ -6,3 +6,10 @@ numeros.unshift(0);
 
 console.log(numeros);
 //output: [ 0, '1', '22', '333', '4444' ]
+
+//exemplo 2:
+let moradias = ["Apartamento", "Casa"];
+moradias.unshift("Condominio");
+
+console.log(moradias);
+//output: [ 'Condominio', 'Apartamento', 'Casa' ]
