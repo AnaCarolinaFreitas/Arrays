381f5e5f10307eb7105265d8abc1ed6eae6cd755
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 19:37:33 -0300

    Este é o metodo shift e seu primeiro exemplo

---
+++ shift.js
@@ -0,0 +1,8 @@
+//Este é o metodo shift
+//Ao utiliza-lo, o primeiro elemento do array é removido
+//Exemplo 1:
+let cantores = ["Taylor Swift", "Harry Styles", "The weekend", "Bruno Mars"];
+cantores.shift();
+
+console.log(cantores);
+//output: [ 'Harry Styles', 'The weekend', 'Bruno Mars' ]
