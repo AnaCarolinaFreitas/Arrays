2ad8621585ca7daa52a117b0e5fb809321382dba
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 19:34:37 -0300

    Este commit contem a explicação do join e seu primeiro exemplo

---
+++ join.js
@@ -0,0 +1,11 @@
+// Este é o metodo join
+//Ele permite unir elementos de um array com uma única string
+//Exemplo 1:
+
+let cores = ["Azul", "Verde", "Amarelo", "Laranja"];
+let resultado = cores.join(", ");
+// o (", ") utilizado junto ao join é utilizado como separador
+
+console.log(resultado);
+//output: Azul, Verde, Amarelo, Laranja.
+
