a1cab9c33d8a879a7da59e652501a808c89850ae
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 20:55:50 -0300

    Aqui está o primeiro exemplo junto a explicação do metodo reverse

---
+++ reverse.js
@@ -0,0 +1,14 @@
+//Este é o metodo reverse
+// ele inverte a array
+//exemplo 1:
+let numeros = ["100", "99", "98", "97", "96"];
+numeros.reverse()
+
+console.log(numeros);
+//output: [ '96', '97', '98', '99', '100' ]
+
+//exemplo 2:
+let letras = ["A", "B", "C", "D", "E"];
+letras.reverse()
+
+console.log(letras);
