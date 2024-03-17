6485950a9348746e5bbad082e0fe142182936190
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 23:29:37 -0300

    Este é o metodo sort, neste commit temos um exemplo de seu funcionamento

---
+++ sort.js
@@ -0,0 +1,10 @@
+//Este é o metodo sort
+//Este metodo converte os elementos array em strings e os ordena
+//Exemplo 1:
+let nomes = ["Ana", "Luiza", "José"];
+//Declarei a variavel original
+nomes.sort();
+//Utilizo o sort para alterar a ordem dos elementos
+
+console.log(nomes);
+//output: [ 'Ana', 'José', 'Luiza' ] 
