dacb6796bf0bcd7921dc55f5a6a260fb91185d35
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 23:15:47 -0300

    Aqui está o metodo length junto a um exemplo

---
+++ length.js
@@ -0,0 +1,11 @@
+//Este é o metodo length
+//O metodo length permite que obtenhamos o tamanho de um array
+//Exemplo 1:
+let marcas = ["LG", "Amazon", "Dior"];
+//Defini a variavel nomeada "marcas"
+let tamanho = marcas.length;
+//defini a variavel, e declarei que desejo saber o tamanho da variavel marcas
+
+console.log("O tamanho é:" +  tamanho);
+//Utilizei o comando console.log e adicionei uma frase que se somará a variavel tamanho definida anteriormente
+//output: O tamanho é:3
