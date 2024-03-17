33173dd238be5c91e79e4864e5be393f3429f25a
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 21:50:03 -0300

    Este é o indexOf, foi colocado o primeiro exemplo junto a explicação

---
+++ indexOf.js
@@ -0,0 +1,11 @@
+//Este é o metodo .indexOf
+// Ele exibe o primeiro index (posição) de um valor especifico
+//exemplo 1:
+let objetos = ["Lampada", "Computador", "Calculadora", "Borracha"];
+//Declarei a variavel objetos
+let index = objetos.indexOf("Calculadora");
+//declarei uma variavel e utilizei o metodo para exibir o index da palavra Calculadora
+
+console.log(index);
+//Utilizado para exibir o index
+//output: 2
