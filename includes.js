cea4d916e8f074cd907d31c09b775ecd5bf2508f
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 22:36:44 -0300

    Neste commit utilizei de um exemplo onde o elemento não existe para demostrar o funcionamento do metodo

--- includes.js
+++ includes.js
@@ -7,6 +7,19 @@ let correto = letras.includes("Y");
 //Defini uma variavel, e utilizei o metodo .includes para descobrir se o elemento Y existe no array
 
 console.log(correto);
-//Utilizei o comando para exibir 
+//Utilizei o comando para exibir o boleano
 //Se o elemento existir, na saida será exibido um boleano true
 //output: true
+
+//Exemplo 2:
+let letters = ["I", "J", "K"];
+//defini a variavel letters
+let falso = letters.includes("L");
+//Neste utilizei o includes para encontrar o elemento "L", que não existe no array
+
+console.log(falso);
+//Utilizei o comando para exibir o boleano
+//Se o elemento não existir, na saida será exibido um boleano false
+//output: false
+
+
