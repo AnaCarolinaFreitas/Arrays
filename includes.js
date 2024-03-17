4e770bc17d385d428b2899dc8e7509c14b21b715
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-16 22:31:38 -0300

    Estou, neste commit, apresentando o metodo 'includes' e exibindo um exemplo

---
+++ includes.js
@@ -0,0 +1,12 @@
+//Este é o metodo includes
+//Ao utiliza-lo é possivel verificar se um elemento existe no Array
+//Exemplo 1:
+let letras = ["X", "Y", "Z"];
+//Utilizei o let para definir a variavel letras
+let correto = letras.includes("Y");
+//Defini uma variavel, e utilizei o metodo .includes para descobrir se o elemento Y existe no array
+
+console.log(correto);
+//Utilizei o comando para exibir 
+//Se o elemento existir, na saida será exibido um boleano true
+//output: true
