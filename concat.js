d417cdfae31623d593a75af69fa89a7450ea2c65
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2024-03-17 00:33:35 -0300

    Utilizando o comando, executei este segundo exemplo

--- concat.js
+++ concat.js
@@ -11,3 +11,12 @@ console.log(outros)
 //Porem quando utilizamos a outra variavel que contem o metodo concat, os elementos são adicionados diretamente na exibição
 //output: [ 'Mercúrio', 'Vênus', 'Terra', 'Júpiter', 'Saturno' ]
 
+//Exemplo 2:
+let princesasDisney = ["Ariel", "Aurora", "Tiana"];
+let outras = princesasDisney.concat(["Bela", "Cinderela"]);
+//O concat foi utilizado para adicionar o nome de mais duas princesas ao array
+
+console.log(princesasDisney);
+//output: [ 'Ariel', 'Aurora', 'Tiana' ]
+console.log(outras);
+//output: [ 'Ariel', 'Aurora', 'Tiana', 'Bela', 'Cinderela' ]
